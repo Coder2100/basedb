c2b4789934c069a62187ae41cf44b3c6abcbaf4c
Coder2100 <apexcurestats@gmail.com>
2020-11-15 16:20:25 +0200

    after psql

---
+++ postgres-express-react-node-tutorial/app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const logger = require('morgan');
+
+const bodyParser = require('body-parser');
+
+//setting up the app
+const app = express();
+//log request to the console
+
+
+app.use(logger('dev'));
+//parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//set up of default page/api end point
+app.get('*', (req, res) => res.status(200).send({
+      message: 'Welcome to your clients services portal.',
+}));
+
+module.exports = app;
